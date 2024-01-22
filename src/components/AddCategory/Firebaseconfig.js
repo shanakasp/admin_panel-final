22ce44820430b4423152c8aaeeab70a920bea603
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-01-23 00:54:05 +0530

    modified Firebaseconfig.js in AddCategory

--- src/components/AddCategory/Firebaseconfig.js
+++ src/components/AddCategory/Firebaseconfig.js
@@ -2,14 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCc2oSxsd0NHStBQ3HQPixhDLpCeE5eVsc",
+  authDomain: "authentication-react-2883c.firebaseapp.com",
+  projectId: "authentication-react-2883c",
+  storageBucket: "authentication-react-2883c.appspot.com",
+  messagingSenderId: "659890734311",
+  appId: "1:659890734311:web:cd903a0c790152d0c70e42",
+  measurementId: "G-FFX6MNVG18",
 };
 
 const app = initializeApp(firebaseConfig);
-export const storage = getStorage(app);
+const storage = getStorage(app);
+
+export { storage };
