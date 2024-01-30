5e1f8a64c7e668e53e295bdfea5e081409584437
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-01-30 19:34:07 +0530

    modified Question.css and Question.jsx in Questions

--- src/components/Questions/Question.jsx
+++ src/components/Questions/Question.jsx
@@ -1,3 +1,5 @@
+import DeleteIcon from "@mui/icons-material/Delete";
+import { Button, Grid, Pagination, Paper, Typography } from "@mui/material";
 import axios from "axios";
 import { MDBTable, MDBTableBody, MDBTableHead } from "mdb-react-ui-kit";
 import React, { useEffect, useState } from "react";
@@ -5,6 +7,8 @@ import { useParams } from "react-router-dom";
 
 function Question() {
   const [allQuestions, setAllQuestions] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [questionsPerPage] = useState(10); // Adjust as needed
   const getParam = useParams();
 
   useEffect(() => {
@@ -22,10 +26,17 @@ function Question() {
       });
   }, []);
 
-  const handleEdit = (questionId) => {
-    console.log(`Edit button clicked for question with ID ${questionId}`);
-  };
+  // Get current questions
+  const indexOfLastQuestion = currentPage * questionsPerPage;
+  const indexOfFirstQuestion = indexOfLastQuestion - questionsPerPage;
+  const currentQuestions = allQuestions
+    .filter((question) => question.category_id == getParam.id)
+    .slice(indexOfFirstQuestion, indexOfLastQuestion);
 
+  // Change page
+  const handleChangePage = (event, newPage) => {
+    setCurrentPage(newPage);
+  };
   const handleDelete = (questionId) => {
     axios
       .delete(`http://localhost:8080/questions/deleteQuestion`, {
@@ -43,110 +54,110 @@ function Question() {
         console.error("Error deleting:", error);
       });
   };
-  return (
-    <div
-      className="QuestionsTable"
-      style={{
-        width: "100%",
-        background: "#fff",
-        overflowY: "scroll",
-        height: "600px",
-        padding: "25px",
-        borderRadius: "10px",
-        margin: "-15px 10px",
-      }}
-    >
-      <MDBTable
-        align="middle"
-        style={{
-          overflowY: "auto",
-          height: "40px",
-          tableLayout: "fixed",
-          marginRight: "10px",
-        }}
-      >
-        <MDBTableHead>
-          <tr style={{ color: "#646a85" }}>
-            <th
-              scope="col"
-              style={{ color: "blueviolet", width: "50%", fontSize: "20px" }}
-            >
-              Title
-            </th>
-            <th
-              scope="col"
-              style={{ color: "blueviolet", width: "45%", fontSize: "20px" }}
-            >
-              Input Type
-            </th>
-            <th
-              scope="col"
-              style={{ color: "blueviolet", width: "36%", fontSize: "20px" }}
-            >
-              Ans.
-            </th>
-            <th
-              scope="col"
-              style={{ color: "blueviolet", width: "30%", fontSize: "20px" }}
-            >
-              Action
-            </th>
-          </tr>
-        </MDBTableHead>
 
-        <MDBTableBody>
-          {Array.isArray(allQuestions) && allQuestions.length > 0 ? (
-            allQuestions
-              .filter((question) => question.category_id == getParam.id)
-              .map((question, index) => (
-                <tr key={index}>
-                  <td>
-                    <p
-                      className="fw-bold mb-1"
-                      style={{ color: "#041083", cursor: "pointer" }}
-                    >
-                      {question.title}
-                    </p>
-                  </td>
-                  <td>
-                    <p
-                      className="fw-bold mb-1"
-                      style={{ color: "#041083", cursor: "pointer" }}
-                    >
-                      {question.type}
-                    </p>
-                  </td>
-                  <td>
-                    <p
-                      className="fw-bold mb-1"
-                      style={{ color: "#041083", cursor: "pointer" }}
-                    >
-                      {question.values.map((value) => value.value).join(", ")}
-                    </p>
-                  </td>
-                  <td>
-                    <button
-                      onClick={() => handleDelete(question.id)}
-                      className="btn btn-danger rounded-pill bg-danger"
-                      style={{
-                        fontSize: "14px",
-                        width: "60px",
-                        padding: "2px",
-                      }}
-                    >
-                      Delete
-                    </button>
-                  </td>
+  return (
+    <Grid container spacing={2} justifyContent="right">
+      <Grid item xs={12}>
+        <Paper
+          className="QuestionsTable"
+          style={{ padding: "15px", borderRadius: "10px" }}
+        >
+          <Typography variant="h5" color="primary" gutterBottom>
+            Questions
+          </Typography>
+          <MDBTable responsive>
+            <MDBTableHead>
+              <tr style={{ color: "#041083" }}>
+                <th scope="col" style={{ color: "#041083", width: "35%" }}>
+                  Title
+                </th>
+                <th scope="col" style={{ color: "#041083" }}>
+                  Input Type
+                </th>
+                <th scope="col" style={{ color: "#041083", width: "50%" }}>
+                  Ans.
+                </th>
+                <th scope="col" style={{ color: "#041083", width: "5%" }}>
+                  Action
+                </th>
+              </tr>
+            </MDBTableHead>
+
+            <MDBTableBody>
+              {Array.isArray(currentQuestions) &&
+              currentQuestions.length > 0 ? (
+                currentQuestions.map((question, index) => (
+                  <tr key={index}>
+                    <td>
+                      <Typography
+                        variant="body1"
+                        style={{
+                          color: "#000000",
+                          cursor: "pointer",
+                          fontSize: "14px",
+                        }}
+                      >
+                        {question.title}
+                      </Typography>
+                    </td>
+                    <td>
+                      <Typography
+                        variant="body1"
+                        style={{
+                          color: "#000000",
+                          cursor: "pointer",
+                          fontSize: "14px",
+                          textTransform: "capitalize",
+                        }}
+                      >
+                        {question.type.charAt(0).toUpperCase() +
+                          question.type.slice(1)}
+                      </Typography>
+                    </td>
+                    <td>
+                      <Typography
+                        variant="body1"
+                        style={{
+                          color: "#000000",
+                          cursor: "pointer",
+                          fontSize: "14px",
+                        }}
+                      >
+                        {question.values.map((value) => value.value).join(", ")}
+                      </Typography>
+                    </td>
+                    <td style={{ display: "flex", gap: "5px" }}>
+                      <Button
+                        onClick={() => handleDelete(question.id)}
+                        variant="contained"
+                        color="error"
+                        startIcon={<DeleteIcon />}
+                        size="small"
+                      ></Button>
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan="4">No questions found.</td>
                 </tr>
-              ))
-          ) : (
-            <tr>
-              <td colSpan="4">No questions found.</td>
-            </tr>
-          )}
-        </MDBTableBody>
-      </MDBTable>
-    </div>
+              )}
+            </MDBTableBody>
+          </MDBTable>
+          <Pagination
+            count={Math.ceil(
+              allQuestions.filter(
+                (question) => question.category_id == getParam.id
+              ).length / questionsPerPage
+            )}
+            page={currentPage}
+            onChange={handleChangePage}
+            color="primary"
+            style={{ marginTop: "20px" }}
+          />
+        </Paper>
+      </Grid>
+    </Grid>
   );
 }
 
